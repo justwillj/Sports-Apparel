7c326e0b62deb5a54dd3cdbad860a7f7e2f851bb
Isabel Lessley <isabelelaine@protonmail.com>
2023-04-04 12:41:00 -0400

    CategoryPage works as intended

--- src/components/category-page/CategoryPage.js
+++ src/components/category-page/CategoryPage.js
@@ -4,7 +4,7 @@ import ProductCard from '../product-card/ProductCard';
 import styles from '../product-page/ProductPage.module.css';
 import Constants from '../../utils/constants';
 import fetchProducts from '../product-page/ProductPageService';
-import constants, { WOMEN_ENDPOINT } from '../../utils/constants';
+import constants, { WOMEN_ENDPOINT, MEN_ENDPOINT, KIDS_ENDPOINT } from '../../utils/constants';
 // import "./CategoryPage.module,css"
 
 //PAGE LAYOUT
@@ -21,39 +21,17 @@ import constants, { WOMEN_ENDPOINT } from '../../utils/constants';
  * @return component
  */
 const CategoryPage = (props) => {
-    const [query, setQuery] = useState('');
   const [products, setProducts] = useState([]);
   const [apiError, setApiError] = useState(false);
   const [department, setDepartment] = useState(props.category);
   const [category, setCategory] =  useState('Running');
-  
-    console.log(department);
-
-//   '/products?demographic=Men&category=Running&type=Short'
-  //conditional rendering for department and category
 
 
   useEffect(() => {
-    if (department == "Men") {
-        setQuery('/products?demographic=Men');
-        //Nested if statement to select category
-    } 
-    if (department == "Women") {
-        setQuery('/products?demographic=Women');
-        //Nested if statement to select category
-    }
-    if (department == "Kids") {
-        setQuery('/products?demographic=Kids');
-        //Nested if statement to select category
-    }
-
-    console.log(query);
-
     async function fetchFilteredProducts(prod, setApiError) {
-      await HttpHelper(query, 'GET')
+      await HttpHelper(`/products?demographic=${props.category}&category=Running`, 'GET')
         .then((response) => {
           if (response.ok) {
-            // setQuery(sessionStorage.getItem('userSearch'));
             return response.json();
           }
           throw new Error(Constants.API_ERROR);
@@ -64,24 +42,6 @@ const CategoryPage = (props) => {
         });
     }
     fetchFilteredProducts(setProducts, setApiError);
-  }, []);
-
-//   async function fetchProducts(setProducts, setApiError) {
-//     await HttpHelper(searchPath, 'GET')
-//       .then((response) => {
-//         if (response.ok) {
-//           return response.json();
-//         }
-//         throw new Error(Constants.API_ERROR);
-//       })
-//       .then(setProducts)
-//       .catch(() => {
-//         setApiError(true);
-//       });
-//   }
-
-  useEffect(() => {
-    fetchProducts(setProducts, setApiError, setDepartment, setCategory);
   }, []);
 
   return (
