416dccb6f499fe1be5052ebcadbe0945512f7a3d
Charles K <charlesknight922@gmail.com>
2023-04-06 11:13:21 -0400

    search modified for partial word matches

--- src/utils/utilFunctions.js
+++ src/utils/utilFunctions.js
@@ -3,24 +3,29 @@ const searchFilter = (products, query) => {
   if (query === '') {
     return products;
   }
-  products.filter((product) => {
-    const prodNameToLower = product.name.toLowerCase();
-    const prodDemToLower = product.demographic.toLowerCase();
-    const prodCatToLower = product.category.toLowerCase();
-    const prodDescToLower = product.description.toLowerCase();
-    const prodTypeToLower = product.type.toLowerCase();
-    const queryToLower = query.toLowerCase();
-    if (
-      prodNameToLower.match(queryToLower)
-      || prodDemToLower.match(queryToLower)
-      || prodCatToLower.match(queryToLower)
-      || prodDescToLower.match(queryToLower)
-      || prodTypeToLower.match(queryToLower)
-    ) {
-      newList.push(product);
-    }
-    return newList;
+  const queryToLower = query.toLowerCase().split(' ');
+  queryToLower.forEach((element) => {
+    products.filter((product) => {
+      const prodNameToLower = product.name.toLowerCase();
+      const prodDemToLower = product.demographic.toLowerCase();
+      const prodCatToLower = product.category.toLowerCase();
+      const prodDescToLower = product.description.toLowerCase();
+      const prodTypeToLower = product.type.toLowerCase();
+      // const queryToLower = query.toLowerCase();
+      if (
+        prodNameToLower.match(element)
+      || prodDemToLower === element
+      || prodCatToLower.match(element)
+      || prodDescToLower.match(element)
+      || prodTypeToLower.match(element)
+      ) {
+        newList.push(product);
+      }
+      return newList;
+    });
   });
+
+  console.log(newList);
   return newList;
 };
 export default searchFilter;
