c31468c558b613891c6a90cc06312c32094d9dcb
Isabel Lessley <isabelelaine@protonmail.com>
2023-04-07 12:46:51 -0400

    fixed some styling

--- src/components/product-modal/ProductModal.js
+++ src/components/product-modal/ProductModal.js
@@ -56,7 +56,7 @@ const ProductModal = ({ open,  product, close }) => {
     const type = product.type;
     const image = {PLACEHOLDER_IMAGE};
     const primaryColor = "Red"
-    const secondaryColor = "Lilac"
+    const secondaryColor = "Blue"
     const description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure"
     const price = "$19.99"
 
@@ -88,7 +88,7 @@ const ProductModal = ({ open,  product, close }) => {
       </div>
       <h2 className='name'>{department} {category} {type}</h2>
       
-      <h4 className='swatchHeader'>Color Choice</h4>
+      <h3 className='swatchHeader'>Color Choice</h3>
       <div className='swatchContainer'>
         <div className= 'colorSwatch' id='primarySwatch' style={{backgroundColor: "Red"}}/>
         <div className= 'colorSwatch' id='secondarySwatch' style={{backgroundColor: "Blue"}}/>
