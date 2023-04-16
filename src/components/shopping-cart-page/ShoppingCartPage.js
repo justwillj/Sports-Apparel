d38c80ee19c0711117b54d9c2bc8b8e0ddde7201
Adrienne W <adrienne.whetsell@gmail.com>
2023-04-16 19:24:55 -0400

    added loading spinner and condiditonal rendering to ShoppingCartPage.js

--- src/components/shopping-cart-page/ShoppingCartPage.js
+++ src/components/shopping-cart-page/ShoppingCartPage.js
@@ -1,7 +1,8 @@
 /* eslint-disable */
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './ShoppingCartPage.css';
 import ShoppingCartItem from '../shopping-cart-item/ShoppingCartItem';
+import styles from '../search/SiteSearch.module.css';
 
 /**
  * @name ShoppingCartPage
@@ -11,9 +12,10 @@ import ShoppingCartItem from '../shopping-cart-item/ShoppingCartItem';
 const ShoppingCartPage = () => {
   const [shoppingCart, setShoppingCart] = useState([]);
   const [products, setProducts] = useState([]);
-
+  const [loading, setLoading] = useState(false);
 
   const getAllData = () => {
+    setLoading(true);
     // Link that helped me with this
     // https://medium.com/@jdhawks/make-fetch-s-happen-5022fcc2ddae
     Promise.all([
@@ -24,6 +26,7 @@ const ShoppingCartPage = () => {
     ])
       .then(([resProd, resShop]) => {
         if (!resProd.ok || !resShop.ok) {
+          setLoading(false);
           throw Error;
         }
         return Promise.all([resProd.json(), resShop.json()]);
@@ -31,7 +34,8 @@ const ShoppingCartPage = () => {
       .then(([dataProd, dataShop]) => {
         setProducts(dataProd);
         setShoppingCart(dataShop);
-      })
+        setLoading(false);
+      });
   };
 
   /**
@@ -47,27 +51,28 @@ const ShoppingCartPage = () => {
 
   // const totalPrice = shoppingCart.reduce((total, x) => total + x.price, 0);
 
-  if (shoppingCart.length === 0) {
-    return (
-      <main className="body">
-        <br />
-        <h2 className="message">Nothing to see here. Add products to your cart to get started</h2>
-      </main>
-    );
-  }
-
   return (
     <main className="body">
       <h1 className="title">Shopping Cart</h1>
-      {shoppingCart.map((shop)=>(
-               <div>
-        {products.map((product)=>
-        <div>
-          {shop.customerId == sessionStorage.getItem("customerId") && shop.productId === product.id ? <ShoppingCartItem key={product.id} product={product} /> :null} 
-      </div>
-          )}
-         </div>
-    ))}
+      {loading && (
+        <div className={styles.ldsContainer}>
+          <div className={styles.ldsDualRing} />
+        </div>
+      )}
+      {!loading && (
+        <main className="body">
+          <br />
+          {shoppingCart.length === 0 ? <h2 className="message">Nothing to see here. Add products to your cart to get started</h2> : shoppingCart.map((shop) => (
+            <div>
+              {products.map((product) => (
+                <div>
+                  {shop.customerId === sessionStorage.getItem('customerId') && shop.productId === product.id ? <ShoppingCartItem key={product.id} product={product} /> : null}
+                </div>
+              ))}
+            </div>
+          ))}
+        </main>
+      )}
     </main>
   );
 };
